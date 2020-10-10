fc5368195e4e4da331e5795cbef13e92eda2b275
Mac Parthum <macparthum@gmail.com>
2020-10-10 14:34:29 -0400

    Updates

---
+++ precache-manifest.29eb7051862ee973073fa661d2416e90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f86aea276f14ceabe09e8db6ede886",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e117c5c926db150e609",
+    "url": "./static/css/main.ee2e6dbf.chunk.css"
+  },
+  {
+    "revision": "2302cd12d4d2b81ca103",
+    "url": "./static/js/2.b3bbf06a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b3bbf06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e117c5c926db150e609",
+    "url": "./static/js/main.12398df1.chunk.js"
+  },
+  {
+    "revision": "fb584ddc8f50b554653a",
+    "url": "./static/js/runtime-main.1f030364.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nasa-apod/precache-manifest.f23ee62c3d340cf095305a7e048209db.js"
+  "./precache-manifest.29eb7051862ee973073fa661d2416e90.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-apod/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
